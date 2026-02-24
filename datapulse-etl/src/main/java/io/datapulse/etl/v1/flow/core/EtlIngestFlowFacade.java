7f64e21be40544f76cfd27e2b7e55303ab5009f1
lckbstrd <vitaliikim144@gmail.com>
2026-02-24 11:46:18 +0400

    Fix ingest facade wiring with explicit qualified constructor

--- datapulse-etl/src/main/java/io/datapulse/etl/v1/flow/core/EtlIngestFlowFacade.java
+++ datapulse-etl/src/main/java/io/datapulse/etl/v1/flow/core/EtlIngestFlowFacade.java
@@ -1,18 +1,21 @@
 package io.datapulse.etl.v1.flow.core;
 
 import io.datapulse.etl.v1.flow.core.EtlSnapshotIngestionFlowConfig.IngestCommand;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class EtlIngestFlowFacade {
 
-  @Qualifier(EtlFlowConstants.CH_ETL_INGEST)
   private final MessageChannel etlIngestChannel;
+
+  public EtlIngestFlowFacade(
+      @Qualifier(EtlFlowConstants.CH_ETL_INGEST) MessageChannel etlIngestChannel
+  ) {
+    this.etlIngestChannel = etlIngestChannel;
+  }
 
   public void ingest(IngestCommand command) {
     etlIngestChannel.send(new GenericMessage<>(command));
